1b70dd5a678347104d4b791d2a95c40cafcfef38
Alokcsit <swainalokkumar25@gmail.com>
2024-08-23 22:25:57 +0530

    initial commit

--- src/Bucketlist.js
+++ src/Bucketlist.js
@@ -1,12 +1,72 @@
 import React from 'react'
-import reducer from './Bucketreducer'
+import {reducer ,reducer1} from './Bucketreducer'
 import { useReducer } from 'react'
+import { v4 as uuidv4 } from "uuid";
 function Bucketlist() {
-let initialstate=[]
+let initialstate={
+activity:"",
+date:""
+}
 const[state,dispatch]=useReducer(reducer,initialstate)
+const [wish,dispatchwish]=useReducer(reducer1,{wish:[]})
+
+const onchangeHandler=(e)=>{
+ const {name,value}=e.target
+  dispatch(
+    {
+      type:'set_input',
+    payload:{
+      name,
+      value
+    }
+    }
+  )
+}
+
+const onclickHandler=()=>{
+const id=uuidv4()
+  dispatchwish(
+    {
+      type:'set_wish',
+      payload:{
+        id,
+        ...state
+      }
+    }
+  )
+}
+const handleComplete = (id) => {
+  dispatchwish({ type: "COMPLETE", payload: id });
+}
+   console.log(state)
   return (
     <div>
-      <input type='text' placeholder='make wish' onChange={(e)=>e.target.value} />
+      <input type='text' name='activity' placeholder='write wish' onChange={onchangeHandler} />
+      <input type='date' name='date' onChange={onchangeHandler}/>
+
+      <br></br>
+      <div>
+        <button onClick={onclickHandler}> make a wish</button>
+      </div>
+      {wish.wish.length > 0 ? (
+        <div>
+          {wish.wish.map((item) => (
+            <div key={item.id} className={item.completed ? "completed" : ""}>
+              <div>
+                Activity: {item.activity}, Date: {item.date}
+              </div>
+              <button onClick={() => handleComplete(item.id)}>Complete</button>
+              <button
+                onClick={() =>
+                  dispatchwish({ type: "DELETE", payload: item.id })
+                }
+              >
+                Delete
+              </button>
+            </div>
+          ))}
+        </div>
+      ) : null}
     </div>
   )
 }
--- src/Bucketreducer.js
+++ src/Bucketreducer.js
@@ -1,4 +1,40 @@
-const reducer=()=>{
+const reducer=(state,action)=>{
+console.log(state,action)
+console.log(action.type)
+console.log(action.payload)
+console.log(action.payload.value)
+console.log("name",action.payload.name)
+ switch(action.type){
+  case 'set_input':
+    return{
+      ...state,
+      [action.payload.name]:action.payload.value
+    }
+  default:
+    return state
+ }
 
 }
-export default reducer;
+const reducer1=(state,action)=>{
+    switch(action.type){
+      case 'set_wish':
+        return{
+          ...state,
+          wish:[...state.wish,action.payload]
+        }
+        case 'DELETE':
+            return{
+              ...state,
+              wish:state.wish.filter((item)=>item.id!==action.payload)
+            }
+            case 'COMPLETE':
+                return{
+                  ...state,
+                  wish:state.wish.filter((item)=>item.id!==action.payload ?{...item,complete:!item.complete}:item)  
+                }
+      default:
+        return state
+     }
+}
+
+export {reducer,reducer1 } ;
